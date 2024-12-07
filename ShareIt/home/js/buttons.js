7ac44ea075c96cc742415e6a60637281c03384f5
oduagbondestiny <143692938+oduagbondestiny@users.noreply.github.com>
2024-12-07 15:58:35 +0000

    Add files via upload

---
+++ ShareIt/home/js/buttons.js
@@ -0,0 +1,42 @@
+
+function onMessages() {
+
+    window.location.href = "messages.html";
+    
+  }
+  function onSettings() {
+  
+    window.location.href = "settings.html";
+    
+  }
+  function onContact() {
+  
+    window.location.href = "../contact.html";
+    
+  }
+  function onBack() {
+
+    window.history.back(-1);
+
+   
+  }
+  function onchangeEmail() {
+
+     window.location.href = "auth/email.html";
+    
+  }
+  function onchangePassword() {
+
+     window.location.href = "auth/password.html";
+    
+  }
+  function onchangeUser() {
+
+     window.location.href = "auth/user.html";
+    
+  }
+function onArchieve() {
+
+    window.location.href= " archieve.html";
+    
+}
